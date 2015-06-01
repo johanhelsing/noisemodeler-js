97d867e8e59b087feb9a85915fb69892ed93ac03
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2015-06-02 00:21:15 +0200

    Add another test.

--- src/compile.spec.js
+++ src/compile.spec.js
@@ -2,7 +2,34 @@ var compile = require('./compile');
 require('chai').should();
 
 describe('compile', function() {
+
     it('should return a function', function() {
         compile({}).should.be.a('function');
     });
+
+    it('can compile a very simple module', function() {
+        var oneModuleType = {
+            moduleTypes: [
+                {
+                    name: "meaningOfLife",
+                    description: "outputs the value 42",
+                    inputs: [],
+                    outputs: [
+                        { name: "constant", source: "c1.value" }
+                    ],
+                    modules: [
+                        {
+                            name: "c1",
+                            type: "constant1",
+                            inputs: { value: [ 0 ] }
+                        }
+                    ]
+                }
+            ]
+        };
+
+        compile(oneModuleType);
+    });
+
 });
+
