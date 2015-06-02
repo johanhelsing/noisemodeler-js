0209437741c2dda3b06439fcff8eb30065799dec
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2015-06-02 19:10:33 +0200

    Refactor ugly code from last night.

--- src/compile.spec.js
+++ src/compile.spec.js
@@ -1,34 +1,34 @@
 var compile = require('./compile');
 require('chai').should();
 
+var meaningOfLife = {
+    moduleTypes: [
+        {
+            name: "meaningOfLife",
+            description: "outputs the value 42",
+            inputs: [],
+            outputs: [
+                { name: "constant", source: "c1.value" }
+            ],
+            modules: [
+                {
+                    name: "c1",
+                    type: "constant1",
+                    inputs: { value: [ 42 ] }
+                }
+            ]
+        }
+    ]
+};
+
 describe('compile', function() {
 
     it('should return a function', function() {
         compile({}).should.be.a('function');
     });
 
-    it('can compile a very simple module', function() {
-        var oneModuleType = {
-            moduleTypes: [
-                {
-                    name: "meaningOfLife",
-                    description: "outputs the value 42",
-                    inputs: [],
-                    outputs: [
-                        { name: "constant", source: "c1.value" }
-                    ],
-                    modules: [
-                        {
-                            name: "c1",
-                            type: "constant1",
-                            inputs: { value: [ 0 ] }
-                        }
-                    ]
-                }
-            ]
-        };
-
-        compile(oneModuleType);
+    it('can compile a very simple module without errors', function() {
+        compile(meaningOfLife);
     });
 
 });
