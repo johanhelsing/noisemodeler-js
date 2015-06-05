1781d2ebea6552b9fdf026e263de08d9bced818d
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2015-06-05 19:35:36 +0200

    Format code
    Make module definitions occupy less space and reduce amount of
    indentation levels.

--- src/compile.spec.js
+++ src/compile.spec.js
@@ -10,25 +10,17 @@ describe('compile', () => {
         });
 
         it('can compile a very simple module without errors', () => {
-            var meaningOfLife = {
-                moduleTypes: [
-                    {
-                        name: 'meaningOfLife',
-                        description: 'outputs the value 42',
-                        inputs: [],
-                        outputs: [
-                            { name: 'meaning', source: 'c1.value' }
-                        ],
-                        modules: [
-                            {
-                                name: 'c1',
-                                type: 'constant1',
-                                inputs: { value: [ 42 ] }
-                            }
-                        ]
-                    }
-                ]
-            };
+            var meaningOfLife = {moduleTypes: [{
+                name: 'meaningOfLife',
+                description: 'outputs the value 42',
+                inputs: [],
+                outputs: [{ name: 'meaning', source: 'c1.value' }],
+                modules: [{
+                    name: 'c1',
+                    type: 'constant1',
+                    inputs: { value: [ 42 ] }
+                }]
+            }]};
 
             compile(meaningOfLife);
         });
@@ -38,80 +30,62 @@ describe('compile', () => {
     describe('constants', () => {
 
         it('creates a function which returns the expected results', () => {
-            var meaningOfLife = {
-                moduleTypes: [
-                    {
-                        name: 'meaningOfLife',
-                        description: 'outputs the value 42',
-                        inputs: [],
-                        outputs: [
-                            { name: 'meaning', source: 'c1.value' }
-                        ],
-                        modules: [
-                            {
-                                name: 'c1',
-                                type: 'constant1',
-                                inputs: { value: [ 42 ] }
-                            }
-                        ]
-                    }
-                ]
-            };
+            var meaningOfLife = {moduleTypes: [{
+                name: 'meaningOfLife',
+                description: 'outputs the value 42',
+                inputs: [],
+                outputs: [
+                    { name: 'meaning', source: 'c1.value' }
+                ],
+                modules: [{
+                    name: 'c1',
+                    type: 'constant1',
+                    inputs: { value: [ 42 ] }
+                }]
+            }]};
             var f = compile(meaningOfLife);
             f().should.deep.equal({meaning: 42});
 
         });
 
         it('handles negative constants', () => {
-            var cold = {
-                moduleTypes: [
-                    {
-                        name: 'cold',
-                        description: '0 kelvin in celsius',
-                        inputs: [],
-                        outputs: [
-                            { name: 'temperature', source: 'c1.value' }
-                        ],
-                        modules: [
-                            {
-                                name: 'c1',
-                                type: 'constant1',
-                                inputs: { value: [ -273 ] }
-                            }
-                        ]
-                    }
-                ]
-            };
+            var cold = {moduleTypes: [{
+                name: 'cold',
+                description: '0 kelvin in celsius',
+                inputs: [],
+                outputs: [{ name: 'temperature', source: 'c1.value' }],
+                modules: [{
+                    name: 'c1',
+                    type: 'constant1',
+                    inputs: { value: [ -273 ] }
+                }]
+            }]};
             var f = compile(cold);
             f().should.deep.equal({temperature: -273});
         });
 
         it('supports modules with multiple outputs.', () => {
-            var multipleOutputs = {
-                moduleTypes: [
+            var multipleOutputs = {moduleTypes: [{
+                name: 'multipleOutputs',
+                description: '',
+                inputs: [],
+                outputs: [
+                    { name: 'out1', source: 'c1.value' },
+                    { name: 'out2', source: 'c2.value' }
+                ],
+                modules: [
                     {
-                        name: 'multipleOutputs',
-                        description: '',
-                        inputs: [],
-                        outputs: [
-                            { name: 'out1', source: 'c1.value' },
-                            { name: 'out2', source: 'c2.value' }
-                        ],
-                        modules: [
-                            {
-                                name: 'c1',
-                                type: 'constant1',
-                                inputs: { value: [ 1 ] }
-                            },
-                            {
-                                name: 'c2',
-                                type: 'constant1',
-                                inputs: { value: [ 2 ] }
-                            }
-                        ]
+                        name: 'c1',
+                        type: 'constant1',
+                        inputs: { value: [ 1 ] }
+                    },
+                    {
+                        name: 'c2',
+                        type: 'constant1',
+                        inputs: { value: [ 2 ] }
                     }
                 ]
-            };
+            }]};
             var f = compile(multipleOutputs);
             f().should.deep.equal({out1: 1, out2: 2});
         });
@@ -121,15 +95,13 @@ describe('compile', () => {
     describe('inputs', () => {
 
         it('can pass a single input to an output', () => {
-            var m = {
-                moduleTypes: [{
-                    name: 'passthrough',
-                    description: 'a single input is mapped to as single output',
-                    inputs: [{ name: 'in1', type: '1f' }],
-                    outputs: [{ name: 'out1', source: 'inputs.in1'}],
-                    modules: []
-                }]
-            };
+            var m = {moduleTypes: [{
+                name: 'passthrough',
+                description: 'a single input is mapped to as single output',
+                inputs: [{ name: 'in1', type: '1f' }],
+                outputs: [{ name: 'out1', source: 'inputs.in1'}],
+                modules: []
+            }]};
             var f = compile(m);
             f({in1: 1}).should.deep.equal({out1: 1});
             f({in1: 2}).should.deep.equal({out1: 2});
@@ -137,21 +109,19 @@ describe('compile', () => {
         });
 
         it('can pass multiple inputs to outputs', () => {
-            var m = {
-                moduleTypes: [{
-                    name: 'passthrough multiple',
-                    description: 'two inputs are mapped to two outputs',
-                    inputs: [
-                        { name: 'in1', type: '1f' },
-                        { name: 'in2', type: '1f' }
-                    ],
-                    outputs: [
-                        { name: 'out1', source: 'inputs.in1'},
-                        { name: 'out2', source: 'inputs.in2'}
-                    ],
-                    modules: []
-                }]
-            };
+            var m = {moduleTypes: [{
+                name: 'passthrough multiple',
+                description: 'two inputs are mapped to two outputs',
+                inputs: [
+                    { name: 'in1', type: '1f' },
+                    { name: 'in2', type: '1f' }
+                ],
+                outputs: [
+                    { name: 'out1', source: 'inputs.in1'},
+                    { name: 'out2', source: 'inputs.in2'}
+                ],
+                modules: []
+            }]};
             var f = compile(m);
             f({in1: 1, in2: 2}).should.deep.equal({out1: 1, out2: 2});
             f({in1: 2, in2: 3}).should.deep.equal({out1: 2, out2: 3});
@@ -159,25 +129,17 @@ describe('compile', () => {
         });
 
         it('can pass inputs through one internal module', () => {
-            var m = {
-                moduleTypes: [{
-                    name: 'passthrough multiple',
-                    description: 'two inputs are mapped to two outputs',
-                    inputs: [
-                        { name: 'in1', type: '1f' },
-                    ],
-                    outputs: [
-                        { name: 'out1', source: 'internal.value'},
-                    ],
-                    modules: [
-                        {
-                            name: 'internal',
-                            type: 'constant1',
-                            inputs: { value: 'inputs.in1' }
-                        }
-                    ]
+            var m = {moduleTypes: [{
+                name: 'passthrough multiple',
+                description: 'two inputs are mapped to two outputs',
+                inputs: [{ name: 'in1', type: '1f' }],
+                outputs: [{ name: 'out1', source: 'c1.value'}],
+                modules: [{
+                    name: 'c1',
+                    type: 'constant1',
+                    inputs: { value: 'inputs.in1' }
                 }]
-            };
+            }]};
             var f = compile(m);
             f({in1: 1}).should.deep.equal({out1: 1});
         });
@@ -187,25 +149,17 @@ describe('compile', () => {
     describe('vectors', () => {
 
         it('outputs vectors when it should', () => {
-            var m = {
-                moduleTypes: [
-                    {
-                        name: 'm',
-                        description: 'has an output that is a two dimensional vector',
-                        inputs: [],
-                        outputs: [
-                            { name: 'out1', source: 'c1.value' }
-                        ],
-                        modules: [
-                            {
-                                name: 'c1',
-                                type: 'constant2',
-                                inputs: { value: [ 42, 12 ] }
-                            }
-                        ]
-                    }
-                ]
-            };
+            var m = {moduleTypes: [{
+                name: 'm',
+                description: 'has an output that is a two dimensional vector',
+                inputs: [],
+                outputs: [{ name: 'out1', source: 'c1.value' }],
+                modules: [{
+                    name: 'c1',
+                    type: 'constant2',
+                    inputs: { value: [ 42, 12 ] }
+                }]
+            }]};
             var f = compile(m);
             f({}).should.deep.equal({out1: [42, 12]});
         });
@@ -214,19 +168,13 @@ describe('compile', () => {
             var m = {moduleTypes: [{
                 name: 'passthrough',
                 description: 'passes through a two-dimensional signal',
-                inputs: [
-                    { name: 'in', type: '2f' },
-                ],
-                outputs: [
-                    { name: 'out', source: 'internal.value' }
-                ],
-                modules: [
-                    {
-                        name: 'internal',
-                        type: 'constant2',
-                        inputs: { value: 'inputs.in' }
-                    }
-                ]
+                inputs: [{ name: 'in', type: '2f' }],
+                outputs: [{ name: 'out', source: 'internal.value' }],
+                modules: [{
+                    name: 'internal',
+                    type: 'constant2',
+                    inputs: { value: 'inputs.in' }
+                }]
             }]};
             var f = compile(m);
             f({in: [42,12]}).should.deep.equal({out: [42, 12]});
